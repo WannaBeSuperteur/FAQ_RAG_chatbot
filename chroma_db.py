5fe790b9f8710ca9138f23a28aa9f59d76f7e738
wannabesuperteur <iwant_optimi@naver.com>
2026-01-30 18:10:33 +0900

    ADD: Chroma DB load function

--- chroma_db.py
+++ chroma_db.py
@@ -1,17 +1,52 @@
 
 import pandas as pd
 from embedding import HFMeanPoolingEmbedder
-from typing import Collection
+import chromadb
+from chromadb.api.models.Collection import Collection
+from tqdm import tqdm
 
 
-def build_or_load_chroma(df:pd.DataFrame, embedder:HFMeanPoolingEmbedder) -> Collection:
+CHROMA_DIR = "chroma_db"                     # Chroma DB save path
+CHROMA_COLLECTION = "smartstore_faq_q_only"  # Chroma DB collection name
+CHROMA_DB_BATCH_SIZE = 256
+
+
+def build_or_load_chroma(faq_df:pd.DataFrame, embedder:HFMeanPoolingEmbedder) -> Collection:
     """
         Arguments:
-            - df       (Pandas DataFrame) : original FAQ DataFrame
+            - faq_df   (Pandas DataFrame) : original FAQ DataFrame
             - embedder (Embedder)         : Embedding Function (default: Mean Pooling)
 
         Returns:
             - (collection) : Chroma DB collection
     """
 
-    raise NotImplementedError
+    client = chromadb.PersistentClient(path=CHROMA_DIR)
+
+    collection = client.get_or_create_collection(
+        name=CHROMA_COLLECTION,
+        embedding_function=embedder,
+        metadata={"hnsw:space": "cosine"},
+    )
+
+    # if collection get successful -> then return this collection
+    if collection.count() > 0:
+        return collection
+
+    ids = [str(i) for i in range(len(faq_df))]
+    documents = faq_df["q_for_rag"].tolist()
+    metadatas = [
+        {"question": faq_df.loc[i, "question"], "answer": faq_df.loc[i, "answer"]}
+        for i in range(len(faq_df))
+    ]
+
+    print(f"[Index] ChromaDB index construction start : N={len(faq_df)}")
+    for i in tqdm(range(0, len(faq_df), CHROMA_DB_BATCH_SIZE)):
+        collection.add(
+            ids=ids[i:i + CHROMA_DB_BATCH_SIZE],
+            documents=documents[i:i + CHROMA_DB_BATCH_SIZE],
+            metadatas=metadatas[i:i + CHROMA_DB_BATCH_SIZE],
+        )
+    print("[Index] ChromaDB index construction finished!!")
+
+    return collection
